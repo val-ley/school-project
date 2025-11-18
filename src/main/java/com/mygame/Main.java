0d2b0930f105730cd08846e7bb20957e14edf867
bulutthecat <egekevindalli@gmail.com>
2025-11-18 13:41:49 -0500

    Added JME JFX translation layer to Gradle Build

--- src/main/java/com/mygame/Main.java
+++ src/main/java/com/mygame/Main.java
@@ -8,6 +8,7 @@ import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
@@ -45,7 +46,7 @@ public class Main extends SimpleApplication {
         bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
         // bulletAppState.setDebugEnable(true); // enables debug mode
-        flyCam.setEnabled(true);
+        flyCam.setEnabled(false);
         // Scene setup
         cam.setFrustumPerspective(45f, (float)cam.getWidth() / cam.getHeight(), 0.01f, 1000f);
         
@@ -80,34 +81,59 @@ public class Main extends SimpleApplication {
         // tp player to room start
         playerControl.warp(new Vector3f(0.1f, 1f, 1.1f));
         
-        //setupKeys();
+        setupKeys();
         
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(-0.5f, -0.5f, -0.5f).normalizeLocal());
         rootNode.addLight(sun);
         
     }
-/*
-    public void setupKeys() {
-        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
-        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
-        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
-        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
-        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
-        
-        inputManager.addListener(actionListener, "Left", "Right", "Up", "Down", "Jump");
-        
-    }
-    
-    private final actionListener() {
-        
-    }
-*/
-    @Override
-    public void simpleUpdate(float tpf) {
-        //TODO: add update code
+
+    private void setupKeys() {
+    inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+    inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+    inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+    inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+    inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+
+    inputManager.addListener(actionListener, "Left", "Right", "Up", "Down", "Jump");
     }
 
+    private final ActionListener actionListener = new ActionListener() {
+        @Override
+        public void onAction(String name, boolean isPressed, float tpf) {
+            if (name.equals("Left")) left = isPressed;
+            if (name.equals("Right")) right = isPressed;
+            if (name.equals("Up")) up = isPressed;
+            if (name.equals("Down")) down = isPressed;
+            if (name.equals("Jump") && isPressed) playerControl.jump();
+        }
+    };
+    
+@Override
+public void simpleUpdate(float tpf) {
+    // 1. Calculate movement direction based on Camera rotation
+    Vector3f camDir = cam.getDirection().clone().multLocal(0.6f);
+    Vector3f camLeft = cam.getLeft().clone().multLocal(0.4f);
+    
+    // We don't want to fly, so zero out the Y (up/down) component
+    camDir.y = 0;
+    camLeft.y = 0;
+
+    walkDirection.set(0, 0, 0);
+    if (left) walkDirection.addLocal(camLeft);
+    if (right) walkDirection.addLocal(camLeft.negate());
+    if (up) walkDirection.addLocal(camDir);
+    if (down) walkDirection.addLocal(camDir.negate());
+
+    // 2. Move the Physics Character
+    playerControl.setWalkDirection(walkDirection.mult(10f)); // 10f is speed
+
+    // 3. Move the Camera to the Player's location
+    // We add a slight offset (+6f Y) so the camera is at "Head" height, not "Feet" height
+    cam.setLocation(playerNode.getWorldTranslation().add(0, 6f, 0));
+}
+    
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
